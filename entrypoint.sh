ba5707eb20ee30e043386f09288c2a1e05be5a8f
thangto <tothangasin@gmail.com>
2025-02-07 13:58:21 +0700

    update config of server

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,5 @@ if [[ -z "$STREAM_URL" || -z "$TWITCH_KEY" ]]; then
 fi
 
 echo "Starting FFmpeg streaming..."
+
 ffmpeg -i "$STREAM_URL" -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -c:a aac -b:a 128k -ar 44100 -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY"
