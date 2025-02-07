7fded7874f8361d78595c3bbca252f4b639f2f37
thangto <tothangasin@gmail.com>
2025-02-07 14:14:29 +0700

    update service infor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Debugging: Print all environment variables
-echo "Checking environment variables..."
-env | grep -E 'STREAM_URL|TWITCH_KEY'
-
 # Ensure required environment variables are set
 if [[ -z "$STREAM_URL" || -z "$TWITCH_KEY" ]]; then
   echo "Error: STREAM_URL or TWITCH_KEY is not set!"
@@ -13,4 +9,9 @@ fi
 
 echo "Starting FFmpeg streaming..."
 
-ffmpeg -i "$STREAM_URL" -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k -c:a aac -b:a 128k -ar 44100 -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY"
+ffmpeg -re -i "$STREAM_URL" \
+  -c:v libx264 -preset fast -b:v 8000k -maxrate 8000k -bufsize 16000k \
+  -c:a aac -b:a 320k -ar 48000 \
+  -pix_fmt yuv420p -g 60 -keyint_min 30 -sc_threshold 0 \
+  -x264opts "nal-hrd=cbr:force-cfr=1" \
+  -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY"
