ffd8e9bd212c4ae9b1ccbdf34aa4ed484ce7f0b4
thangto <tothangasin@gmail.com>
2025-02-11 13:01:19 +0700

    update live stream

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,18 @@ while true; do
   echo "Checking if Twitch is receiving the stream..."
 
   if ! ffprobe -i "rtmp://live.twitch.tv/app/$TWITCH_KEY" -show_streams -loglevel error > /dev/null 2>&1; then
-    echo "Twitch stream is down. Restarting FFmpeg..."
+    echo "Twitch stream is down. Stopping any existing FFmpeg processes and restarting stream..."
 
-    pkill -9 ffmpeg || true  # Kill any existing ffmpeg process
+    # Kill any existing ffmpeg processes
+    pkill -9 ffmpeg || true
 
+    # Wait a few seconds to ensure ffmpeg has fully terminated
+    sleep 5
+
+    # Start the new FFmpeg stream with the logo overlay
     ffmpeg -re -i "$STREAM_URL" \
       -i https://1361694720.rsc.cdn77.org/logo/logo-talknow.png \
-      -filter_complex "[0:v][1:v] overlay=10:10" \
+      -filter_complex "[1:v]scale=iw*0.1:ih*0.1[logo];[0:v][logo]overlay=10:10" \
       -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
       -c:a aac -b:a 128k -ar 44100 \
       -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY" \
