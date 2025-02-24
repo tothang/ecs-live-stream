5c89d9ce1c13532ee2eeb7632c0b8eca318f4172
thangto <tothangasin@gmail.com>
2025-02-24 12:38:26 +0700

    Update readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,29 +11,29 @@ fi
 
 echo "Starting FFmpeg streaming..."
 
-#while true; do
-#  echo "Checking if Twitch is receiving the stream..."
-#
-#  if ! ffprobe -i "rtmp://live.twitch.tv/app/$TWITCH_KEY" -show_streams -loglevel error > /dev/null 2>&1; then
-#    echo "Twitch stream is down. Restarting FFmpeg..."
-#
-#    pkill -9 ffmpeg || true  # Kill any existing ffmpeg process
-#
-#    ffmpeg -re -i "$STREAM_URL" \
-#      -i https://1361694720.rsc.cdn77.org/logo/logo-talknow.png \
-#      -filter_complex "[1:v]scale=iw*0.1:ih*0.1[logo];[0:v][logo]overlay=10:10" \
-#      -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
-#      -c:a aac -b:a 128k -ar 44100 \
-#      -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY" \
-#      -rtmp_live persist \
-#      -loglevel error &
-#
-#    pid=$!
-#    echo "FFmpeg started with PID: $pid"
-#    wait $pid
-#  else
-#    echo "Twitch is live. Checking again in 30 seconds..."
-#  fi
-#
-#  sleep 30
-#done
+while true; do
+  echo "Checking if Twitch is receiving the stream..."
+
+  if ! ffprobe -i "rtmp://live.twitch.tv/app/$TWITCH_KEY" -show_streams -loglevel error > /dev/null 2>&1; then
+    echo "Twitch stream is down. Restarting FFmpeg..."
+
+    pkill -9 ffmpeg || true  # Kill any existing ffmpeg process
+
+    ffmpeg -re -i "$STREAM_URL" \
+      -i https://1361694720.rsc.cdn77.org/logo/logo-talknow.png \
+      -filter_complex "[1:v]scale=iw*0.1:ih*0.1[logo];[0:v][logo]overlay=10:10" \
+      -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
+      -c:a aac -b:a 128k -ar 44100 \
+      -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY" \
+      -rtmp_live persist \
+      -loglevel error &
+
+    pid=$!
+    echo "FFmpeg started with PID: $pid"
+    wait $pid
+  else
+    echo "Twitch is live. Checking again in 30 seconds..."
+  fi
+
+  sleep 30
+done
